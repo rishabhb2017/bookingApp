7313636a8ef824a15554a8aa10074b87a373a6b9
rishabhb2017 <74544861+rishabhb2017@users.noreply.github.com>
2022-11-09 11:20:56 +0530

    Update api.js
    updating comments

--- src/server/api.js
+++ src/server/api.js
@@ -14,31 +14,6 @@ export default {
             setTimeout(resolve(data), 1000);
         });
     },
-
-    // update: async function (data) {
-    //     return new Promise((resolve) => {
-    //         const { id, ...rest } = data;
-    //         const targetIndex = Posts.findIndex(x => x.id === id)
-    //         if (targetIndex > -1) {
-    //             Posts[targetIndex] = { ...data }
-    //         } else {
-    //             new Error("Failed to update.")
-    //         }
-    //         setTimeout(resolve(data), 1000);
-    //     });
-    // },
-    // delete: async function (id) {
-    //     return new Promise((resolve) => {
-    //         const targetIndex = Posts.findIndex(x => x.id === id)
-    //         if (targetIndex > -1)
-    //             Posts.splice(targetIndex, 1);
-    //         else {
-    //             new Error("Failed to delete.")
-    //         }
-    //         setTimeout(resolve({ affected: 1 }), 1000);
-    //     });
-    // },
-
     getAll: async function () {
         debugger;
         return new Promise((resolve) => {
